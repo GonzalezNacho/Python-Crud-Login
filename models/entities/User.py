dfdfa9e292e5a8ed2be54c2115dd4740f9ad311f
GonzalezNacho <nacho.ldm63@gmail.com>
2022-11-11 14:53:14 -0300

    clase usario

--- models/entities/User.py
+++ models/entities/User.py
@@ -1,3 +1,13 @@
-class User():
+from werkzeug.security import check_password_hash
 
-    def __init__(self, id, username, password,fullname="") :
+class User(): 
+
+    def __init__(self, id, username, password,fullname=""):
+        self.id = id
+        self.username = username
+        self.password = password
+        self.fullname = fullname
+
+    @classmethod
+    def check_password(self, hashed_password, password):
+        return check_password_hash(hashed_password, password)
