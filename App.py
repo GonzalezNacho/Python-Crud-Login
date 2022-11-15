b677ad7347bb64d5e4f7fbb843eb078c8b61fad7
GonzalezNacho <nacho.ldm63@gmail.com>
2022-11-15 13:37:54 -0300

    cambios en la base de datos y el front end

--- App.py
+++ App.py
@@ -30,7 +30,6 @@ def index():
     return redirect(url_for('Home'))
 
 @app.route('/home')
-#@login_required
 def Home():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM products')
@@ -39,18 +38,21 @@ def Home():
     
 
 @app.route('/add_product', methods =['POST'])
+@login_required
 def add_product():
     if request.method == 'POST':
         name = request.form['name']
         description = request.form['description']
         discount = request.form['discount']
+        discountCode = request.form['discountCode']
         cur = mysql.connection.cursor()
-        cur.execute('INSERT INTO products (name, description, discount) VALUES (%s,%s,%s);',(name, description, discount))
+        cur.execute('INSERT INTO products (name, description, discount, discount_code) VALUES (%s,%s,%s, %s);',(name, description, discount, discountCode))
         mysql.connection.commit()
         flash('Product Added successfully')
         return redirect(url_for('Home'))
 
 @app.route('/edit/<id>')
+@login_required
 def get_product(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM products WHERE id = %s', (id))
@@ -58,24 +60,28 @@ def get_product(id):
     return render_template('edit-product.html', product = data[0])
 
 @app.route('/update/<string:id>', methods = ['POST'])
+@login_required
 def update_product(id):
     if request.method == 'POST':
         name = request.form['name']
         description = request.form['description']
         discount = request.form['discount']
+        discountCode = request.form['discountCode']
         cur = mysql.connection.cursor()
         cur.execute("""
             UPDATE products
             SET name = %s,
                 discount = %s,
-                description = %s
+                description = %s,
+                discount_code = %s
             WHERE id = %s; 
-        """, (name, discount, description, id))
+        """, (name, discount, description, discountCode, id))
         mysql.connection.commit()
         flash('Product Update Successfully')
         return redirect(url_for('Home'))
 
 @app.route('/delete/<string:id>')
+@login_required
 def delete_product(id):
     cur = mysql.connection.cursor()
     cur.execute('DELETE FROM products WHERE id = {0};'.format(id))
@@ -93,14 +99,16 @@ def login():
         if loggedUser != None:
             if loggedUser.password:
                 login_user(loggedUser)
-                return redirect(url_for('Home'))
+                pagina = redirect(url_for('Home'))
             else:
                 flash("Invalid password ...")
-                return render_template('login.html')    
+                pagina = render_template('login.html')    
         else:
             flash("User not found ...")
-            return render_template('login.html')
-    return render_template('login.html')
+            pagina = render_template('login.html')
+    else:
+        pagina = render_template('login.html')
+    return pagina
     
 @app.route('/logout')
 def logout():
@@ -113,8 +121,8 @@ def status401(error):
 def status404(error):
     return "<h1>Página no encontrada</h1>", 404
 
+app.register_error_handler(401, status401)
+app.register_error_handler(404, status404)
     
 if __name__ == '__main__':
-    app.register_error_handler(401, status401)
-    app.register_error_handler(404, status404)
     app.run(port = 3000, debug = True)
--- sql/offprice.sql
+++ sql/offprice.sql
@@ -1,15 +1,15 @@
-CREATE DATABASE  IF NOT EXISTS `flaskCrud` /*!40100 DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `flaskCrud` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `flaskCrud`;
--- MySQL dump 10.13  Distrib 8.0.31, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.40, for Linux (x86_64)
 --
 -- Host: localhost    Database: flaskCrud
 -- ------------------------------------------------------
--- Server version	8.0.31-0ubuntu0.20.04.1
+-- Server version	5.7.40-0ubuntu0.18.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -23,16 +23,17 @@ USE `flaskCrud`;
 
 DROP TABLE IF EXISTS `products`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `products` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
-  `description` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
-  `discount` int DEFAULT NULL,
-  `path_img` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `discount` int(11) NOT NULL,
+  `path_img` varchar(255) COLLATE utf8_unicode_ci DEFAULT '../static/img/LogoFondoBlanco.png',
+  `discount_code` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -50,14 +51,14 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
-  `id` smallint unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
-  `password` char(102) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
-  `fullname` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `password` char(102) COLLATE utf8_unicode_ci NOT NULL,
+  `fullname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci COMMENT='Tabla de usuarios';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Tabla de usuarios';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -79,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-14 17:58:35
+-- Dump completed on 2022-11-15  8:41:31
